3a848c6c2db9e633d3418ac0e56f266485ef30cd
Lukasz Gawron <gawronlucas@gmail.com>
2024-05-07 01:45:45 +0200

    Add option to get parsed pubspec

--- lib/src/project/project_pubspec.dart
+++ lib/src/project/project_pubspec.dart
@@ -1,17 +1,19 @@
-import 'dart:io';
-
+import 'package:pubspec_parse/pubspec_parse.dart';
 import 'package:yaml_edit/yaml_edit.dart';
 
-import 'project.dart';
+import 'project_paths.dart';
 
 String? _pubspecRawContent;
 
 /// Returns the [YamlEditor] for the pubspec.yaml file.
+/// 
+/// To get the parsed [Pubspec], use [pubspec].
+/// 
+/// To save the changes, use [savePubspecToFile].
 YamlEditor get pubspecEditor {
   if (_pubspecRawContent != null) {
     return YamlEditor(_pubspecRawContent!);
   }
-  final pubspecFile = File('$projectDirectoryPath/pubspec.yaml');
   _pubspecRawContent = pubspecFile.readAsStringSync();
   return YamlEditor(_pubspecRawContent!);
 }
@@ -20,8 +22,18 @@ YamlEditor get pubspecEditor {
 ///
 /// Overwrites the file.
 void savePubspecToFile(YamlEditor pubspec) {
-  final pubspecFile = File('$projectDirectoryPath/pubspec.yaml');
   final pubspecContent = pubspec.toString();
   pubspecFile.writeAsStringSync(pubspecContent);
   _pubspecRawContent = pubspecContent;
+}
+
+/// Returns the parsed [Pubspec] (read-only) from the pubspec.yaml file.
+/// 
+/// To modify the pubspec, use [pubspecEditor].
+Pubspec get pubspec {
+  if (_pubspecRawContent != null) {
+    return Pubspec.parse(_pubspecRawContent!);
+  }
+  final pubspecContent = pubspecFile.readAsStringSync();
+  return Pubspec.parse(pubspecContent);
 }
